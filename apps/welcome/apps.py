d5eda155586f4597a01100174b6201b6f6ddbe6f
Mkhmt Core <mkhmtcore@gmail.com>
2025-09-29 19:32:14 +0500

    refactor: added templates dir

--- apps/welcome/apps.py
+++ apps/welcome/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class WelcomeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.welcome'
+    default_auto_field = "django.db.models.BigAutoField"  # type: ignore
+    name = "apps.welcome"
